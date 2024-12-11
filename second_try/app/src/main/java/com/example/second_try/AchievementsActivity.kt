cb44b4acd941738acdf511ed5b5910a44849fefb
BOTYA <danilk1102@gmail.com>
2024-12-11 16:21:22 +0400

    version 0.9

--- second_try/app/src/main/java/com/example/second_try/AchievementsActivity.kt
+++ second_try/app/src/main/java/com/example/second_try/AchievementsActivity.kt
@@ -1,10 +1,12 @@
 package com.example.second_try
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.charts.LineChart
@@ -12,6 +14,7 @@ import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -21,6 +24,7 @@ class AchievementsActivity : AppCompatActivity() {
 
     private lateinit var lineChart: LineChart
     private val database = FirebaseDatabase.getInstance("https://mental-health-72105-default-rtdb.europe-west1.firebasedatabase.app")
+    private val auth = FirebaseAuth.getInstance()
 
     private lateinit var popupContainer: FrameLayout
 
@@ -33,13 +37,26 @@ class AchievementsActivity : AppCompatActivity() {
         popupContainer = findViewById(R.id.popupContainer)
         val dialogTextView: TextView = findViewById(R.id.dialogTextView)
         val showDialogButton: Button = findViewById(R.id.showDialogButton)
+        val backButton: Button = findViewById(R.id.backButton)
+        val closeButton: ImageButton = findViewById(R.id.closeButton)
 
         // Установка текста всплывающего окна
         dialogTextView.text = getString(R.string.dialog_content)
 
+        // Переход на MainActivity
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         // Показ и скрытие окна
         showDialogButton.setOnClickListener {
+            loadMoodReport(dialogTextView)
             popupContainer.visibility = View.VISIBLE
+        }
+        closeButton.setOnClickListener {
+            popupContainer.visibility = View.GONE
         }
         popupContainer.setOnClickListener {
             popupContainer.visibility = View.GONE
@@ -50,39 +67,77 @@ class AchievementsActivity : AppCompatActivity() {
     }
 
     private fun loadChartData() {
-        val chartDataRef = database.getReference("chartData")
-        chartDataRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val entries = mutableListOf<Entry>()
-                for (dataSnapshot in snapshot.children) {
-                    val year = dataSnapshot.child("year").getValue(Float::class.java)
-                    val value = dataSnapshot.child("value").getValue(Float::class.java)
-                    if (year != null && value != null) {
-                        entries.add(Entry(year, value))
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val userId = currentUser.uid
+            val chartDataRef = database.getReference("chartData").child(userId)
+            chartDataRef.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val entries = mutableListOf<Entry>()
+                    for (weekSnapshot in snapshot.children) {
+                        val week = weekSnapshot.key?.toFloatOrNull()
+                        val value = weekSnapshot.getValue(Float::class.java)
+                        if (week != null && value != null) {
+                            entries.add(Entry(week, value))
+                        }
+                    }
+                    if (entries.isNotEmpty()) {
+                        val dataSet = LineDataSet(entries, "График").apply {
+                            lineWidth = 2f
+                            color = Color.BLUE
+                            setDrawCircles(false)
+                            setDrawValues(false)
+                        }
+                        lineChart.data = LineData(dataSet)
+                        lineChart.xAxis.apply {
+                            position = XAxis.XAxisPosition.BOTTOM
+                            granularity = 1f
+                            textColor = Color.BLACK
+                        }
+                        lineChart.axisLeft.textColor = Color.BLACK
+                        lineChart.axisRight.isEnabled = false
+                        lineChart.invalidate()
                     }
                 }
-                if (entries.isNotEmpty()) {
-                    val dataSet = LineDataSet(entries, "График").apply {
-                        lineWidth = 2f
-                        color = Color.BLUE
-                        setDrawCircles(false)
-                        setDrawValues(false)
-                    }
-                    lineChart.data = LineData(dataSet)
-                    lineChart.xAxis.apply {
-                        position = XAxis.XAxisPosition.BOTTOM
-                        granularity = 1f
-                        textColor = Color.BLACK
-                    }
-                    lineChart.axisLeft.textColor = Color.BLACK
-                    lineChart.axisRight.isEnabled = false
-                    lineChart.invalidate()
-                }
-            }
 
-            override fun onCancelled(error: DatabaseError) {
-                error.toException().printStackTrace()
-            }
-        })
+                override fun onCancelled(error: DatabaseError) {
+                    error.toException().printStackTrace()
+                }
+            })
+        }
+    }
+
+    private fun loadMoodReport(dialogTextView: TextView) {
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val userId = currentUser.uid
+            val chartDataRef = database.getReference("chartData").child(userId)
+
+            chartDataRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val moodReport = StringBuilder()
+
+                    for (weekSnapshot in snapshot.children) {
+                        val week = weekSnapshot.key?.toIntOrNull()
+                        val value = weekSnapshot.getValue(Float::class.java)
+                        if (week != null && value != null) {
+                            val moodDescription = when {
+                                value > 5 -> "отличное настроение"
+                                value == 5f -> "среднее настроение"
+                                value > 3 -> "не очень хорошее настроение"
+                                else -> "очень плохое настроение"
+                            }
+                            moodReport.append("В $week неделю у вас было $moodDescription.\n")
+                        }
+                    }
+
+                    dialogTextView.text = moodReport.toString()
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    error.toException().printStackTrace()
+                }
+            })
+        }
     }
 }
--- second_try/app/src/main/java/com/example/second_try/MainActivity.kt
+++ second_try/app/src/main/java/com/example/second_try/MainActivity.kt
@@ -3,33 +3,45 @@ package com.example.second_try
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
+    private lateinit var userRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance("https://mental-health-72105-default-rtdb.europe-west1.firebasedatabase.app/")
 
         // Проверка авторизации пользователя
         if (auth.currentUser == null) {
             startActivity(Intent(this, LoginActivity::class.java))
-            finish() // Закрываем MainActivity, если пользователь не авторизован
+            finish()
         } else {
-            // Подключение XML разметки
             setContentView(R.layout.activity_main)
+
+            // Ссылка на данные текущего пользователя
+            val currentUser = auth.currentUser
+            val userId = currentUser?.uid
+            if (userId != null) {
+                userRef = database.getReference("users").child(userId)
+                loadUserName()
+            }
 
             // Обработчики кнопок
             findViewById<Button>(R.id.btnSchedule).setOnClickListener {
-
+                // Ваш код для кнопки "Расписание"
             }
 
             findViewById<Button>(R.id.btnDiary).setOnClickListener {
-
+                // Ваш код для кнопки "Дневник"
             }
 
             findViewById<Button>(R.id.btnSetGoal).setOnClickListener {
@@ -37,15 +49,46 @@ class MainActivity : AppCompatActivity() {
             }
 
             findViewById<Button>(R.id.btnTips).setOnClickListener {
-
+                // Ваш код для кнопки "Советы"
             }
 
             findViewById<Button>(R.id.btnLogout).setOnClickListener {
                 auth.signOut()
                 startActivity(Intent(this, LoginActivity::class.java))
                 finish()
-
             }
         }
     }
+
+    private fun loadUserName() {
+        val greetingText: TextView = findViewById(R.id.greetingText)
+
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser == null) {
+            greetingText.text = "Добрый день, Пользователь"
+            return
+        }
+
+        // Инициализация FirebaseDatabase с правильным URL
+        val database = FirebaseDatabase.getInstance("https://mental-health-72105-default-rtdb.europe-west1.firebasedatabase.app")
+        val userRef = database.getReference("Users").child(currentUser.uid)
+
+        // Считывание имени пользователя
+        userRef.child("username").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val userName = snapshot.getValue(String::class.java)
+                if (!userName.isNullOrEmpty()) {
+                    greetingText.text = "Добрый день, $userName"
+                } else {
+                    greetingText.text = "Добрый день, Пользователь"
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                greetingText.text = "Добрый день, Пользователь"
+                error.toException().printStackTrace()
+            }
+        })
+    }
+
 }
